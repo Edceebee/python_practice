d4d82c82d0fc011d972a9161b02f4934d52dec2a
Chinonso Edozie <edceebee@gmail.com>
2021-06-06 22:16:55 +0100

    exercise contd

--- controlStatement/largestNumber.py
+++ controlStatement/largestNumber.py
@@ -1,5 +1,5 @@
-# (Find the Largest Number) The process of finding the largest value is used frequently in com-puter
-# applications. For example, a program that determines the winner of a sales contest would inputthe number of units
+# (Find the Largest Number) The process of finding the largest value is used frequently in computer
+# applications. For example, a program that determines the winner of a sales contest would input the number of units
 # sold by each salesperson. The salesperson who sells the most units wins the con-test. Write a pseudocode program
 # , then a Java application that inputs a series of 10 integers and deter-mines and prints the largest integer.
 
